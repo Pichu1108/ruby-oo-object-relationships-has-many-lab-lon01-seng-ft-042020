96799336409f8a209959866e80c38818f30a5316
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-27 11:08:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist
-attr_accessor :name,
+attr_accessor :name
 @@all = []
     def initialize(name)
         @name = name
@@ -22,4 +22,4 @@ attr_accessor :name,
     def self.song_count
         Song.all.size
     end
-en
+end
